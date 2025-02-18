e48cf312331cd2b8ace331b587e1a5cd55f48a71
webLion200 <renzhiwei2014@outlook.com>
2025-02-18 03:34:37 -0500

    引入shadcn/ui

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,8 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+
 import { useActionState } from "react";
 import login from "./actions";
 
@@ -6,36 +10,45 @@ export default function LoginPage() {
   const [errorMessage, formAction] = useActionState(login, undefined);
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">登录</h1>
-      <form action={formAction} className="space-y-4">
-        <input
-          type="email"
-          name="email"
-          placeholder="邮箱"
-          className="border p-2 w-full"
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="密码"
-          className="border p-2 w-full"
-        />
-        <button type="submit" className="bg-blue-500 text-white p-2 w-full">
-          登录
-        </button>
-        <div
-          className="flex h-8 items-end space-x-1"
-          aria-live="polite"
-          aria-atomic="true"
-        >
-          {errorMessage && (
-            <>
-              <p className="text-sm text-red-500">{errorMessage}</p>
-            </>
-          )}
-        </div>
-      </form>
+    <div className="h-lvh flex justify-center items-center">
+      <Card className="w-xl pt-10">
+        <CardTitle className="text-center">登录页</CardTitle>
+        <CardContent className="mt-10">
+          <form action={formAction} className="space-y-4">
+            <Input
+              type="email"
+              name="email"
+              placeholder="请输入邮箱"
+              className="border p-2 w-full"
+            />
+            <Input
+              type="password"
+              name="password"
+              placeholder="请输入密码"
+              className="border p-2 w-full"
+            />
+            <Button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-400 cursor-pointer text-white p-2 w-full"
+            >
+              登录
+            </Button>
+          </form>
+        </CardContent>
+        <CardFooter>
+          <div
+            className="flex h-8 items-end space-x-1"
+            aria-live="polite"
+            aria-atomic="true"
+          >
+            {errorMessage && (
+              <>
+                <p className="text-sm text-red-500">{errorMessage}</p>
+              </>
+            )}
+          </div>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
