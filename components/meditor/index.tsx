6d4e43171c46402d251f8afd65c2a1dde985688a
webLion200 <renzhiwei2014@outlook.com>
2025-02-21 04:09:08 -0500

    上传图片

--- components/meditor/index.tsx
+++ components/meditor/index.tsx
@@ -5,7 +5,7 @@ import "./prosemirror.css";
 import { EditorContent, JSONContent, useEditor } from "@tiptap/react";
 import { useDebouncedCallback } from "use-debounce";
 import { Editor as EditorClass } from "@tiptap/core";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { defaultExtensions } from "./extensions";
 import { ImageUp } from "lucide-react";
 interface IEditor {
@@ -42,28 +42,12 @@ export default function Editor({
         <h2>This is a 2nd level heading</h2>
         <h3>This is a 3rd level heading</h3>
         <h4>This 4th level heading will be converted to a paragraph, because levels are configured to be only 1, 2 or 3.</h4>
-      <img src="https://img2022.cnblogs.com/blog/1216658/202203/1216658-20220324200930356-377823026.png" alt="" width="200" height="300"/>
         `,
     onUpdate: (e) => {
       debouncedUpdates(e);
     },
     immediatelyRender: false,
   });
-
-  const addImage = useCallback(() => {
-    console.log("addImage", editor);
-    // const url = window.prompt("URL");
-    // debugger;
-    // if (url) {
-    // }
-    editor
-      ?.chain()
-      .focus()
-      .setImage({
-        src: "https://iskzuogcmkzumadqjnbv.supabase.co/storage/v1/object/public/posts/banner/banners.png",
-      })
-      .run();
-  }, [editor]);
 
   // hydrate the editor with the defaultValue.
   useEffect(() => {
@@ -74,7 +58,32 @@ export default function Editor({
     }
   }, [editor, defaultValue]);
 
-  if (!editor) return;
+  // 上传图片函数
+  const handleImageChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const formData = new FormData();
+      formData.append("image", e.target.files?.[0]);
+      try {
+        const response = await fetch("/api/upload", {
+          method: "POST",
+          body: formData,
+        });
+        const { url } = await response.json();
+        // 插入图片时使用绝对路径
+        setTimeout(() => {
+          editor
+            ?.chain()
+            .focus()
+            .setImage({
+              src: url,
+            })
+            .run();
+        }, 100);
+      } catch (error) {
+        console.error("Error uploading image:", error);
+      }
+    }
+  };
 
   return (
     <div
@@ -84,7 +93,13 @@ export default function Editor({
       className="relative min-h-[500px] w-full max-w-screen-lg border-stone-200 bg-white sm:mb-[calc(20vh)] sm:rounded-lg sm:border sm:shadow-lg"
     >
       <div className="control-group">
-        <ImageUp />
+        <input
+          type="file"
+          accept="image/*"
+          name="image"
+          onChange={handleImageChange}
+          className="border p-2"
+        />
       </div>
       <EditorContent editor={editor} />
     </div>
